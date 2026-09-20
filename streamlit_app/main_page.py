68fcbe62cd100ad1269df584d75d98f02963b2b0
cowardclaire <cowardclaire@hotmail.co.uk>
2026-09-20 11:25:02 +0100

    updating spelling in my welcome message on main page streamlit app section

--- streamlit_app/main_page.py
+++ streamlit_app/main_page.py
@@ -7,8 +7,8 @@ st.write("""
 Welcome! This dashboard gives you an interactive way to explore retail sales 
 behaviour and understand what really drives product performance.
 
-Dive into the EDA to see how price, promotion, category, and shelf position 
-influence sales. Then head to the prediction page to experiment with different 
+Dive into the EDA (exploratory data analysis) to see how price, promotion, category, and shelf position 
+influence sales. Then head to the Model Predictions page to experiment with different 
 inputs and instantly see how they affect expected sales volume using our 
 XGBoost model.
 
